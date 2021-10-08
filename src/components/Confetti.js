bd637a591039aa7eef71ba29032c3b0eea69dd95
Maxwell Countryman Skewes <maxskewes@gmail.com>
2021-10-07 17:49:35 -0700

    confetti to 'added to CTI' button
    Added confetti effect when users add their project. Added 'Confetti' component and updated 'Bottom Section' of Tag Creator. Closes #808

--- src/components/Confetti.js
+++ src/components/Confetti.js
@@ -22,56 +22,8 @@ const colors = [DARK_BLUE, TEAL, LIGHT_BLUE, LIGHT_BLUE_VAR, YELLOW, WHITE];
 export default class Confetti extends React.Component {
   constructor(props) {
     super(props);
-    this.animationInstance = null;
     this.props = props;
   }
-
-//   makeShot = (particleRatio, opts) => {
-//     this.animationInstance &&
-//       this.animationInstance({
-//         ...opts,
-//         origin: { y: 0.7 },
-//         particleCount: Math.floor(200 * particleRatio),
-//       });
-//   };
-
-  //   fire = () => {
-  // this.makeShot(0.25, {
-  //   spread: 26,
-  //   startVelocity: 55,
-  // });
-
-  // this.makeShot(0.2, {
-  //   spread: 60,
-  // });
-
-  // this.makeShot(0.35, {
-  //   spread: 100,
-  //   decay: 0.91,
-  //   scalar: 0.8,
-  // });
-
-  // this.makeShot(0.1, {
-  //   spread: 120,
-  //   startVelocity: 25,
-  //   decay: 0.92,
-  //   scalar: 1.2,
-  // });
-
-  // this.makeShot(0.1, {
-  //   spread: 120,
-  //   startVelocity: 45,
-  // });
-  //   }
-
-  //   handlerFire = (e) => {
-  //       console.log(e);
-  //     this.fire();
-  //   };
-
-//   getInstance = (instance) => {
-//     this.animationInstance = instance;
-//   };
 
   render() {
     return (
