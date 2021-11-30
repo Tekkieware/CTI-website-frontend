dd17173b4626d410d9e6b53a62679eec1c7a1adb
Maxwell Countryman Skewes <maxskewes@gmail.com>
2021-11-29 17:55:30 -0800

    cleaned About page

--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,19 +1,6 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
 const useStyles = makeStyles((theme) => ({
-  subHeaderStyle: {
-    fontWeight: '400',
-    textAlign: 'center',
-    [theme.breakpoints.down('sm')]: {
-      padding: '48px 30px',
-    },
-    [theme.breakpoints.up('sm')]: {
-      padding: '48px 100px',
-    },
-    [theme.breakpoints.up('md')]: {
-      padding: '48px 200px',
-    },
-  },
   frameStyle: {
     border: 'none',
     overflow: 'hidden',
